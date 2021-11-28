87d20b8a10b5d4ae48fe8619aec16d6dd147e4a8
Anderson Moura <86577024+AndersonMoura333@users.noreply.github.com>
2021-11-28 12:37:37 -0300

    ajustando deploy

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,11 +1,11 @@
 import { connect } from 'mongoose';
 
 
-const uri = 'mongodb+srv://AdminAirbnb:123789@cluster0.vyak2.mongodb.net/Airbnb?retryWrites=true&w=majority';
+const uri = process.env.MONGO_URI;
 
 export async function connecting() {
     try {
-        const cliente = await connect(uri);
+        const cliente = await connect(uri!);
         console.log('Conectado ao MongoDb Atlas');
 
     } catch (error) {
