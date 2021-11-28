a8857675c850930d9711c4a8a5de36348c67a688
Anderson Moura <86577024+AndersonMoura333@users.noreply.github.com>
2021-11-28 17:38:23 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import app from './app';
 
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(app.get('port'), () => {
     console.log('Express na porta: ', app.get('port'));
     console.log('Express no modo: ', app.get('env'));
 })
