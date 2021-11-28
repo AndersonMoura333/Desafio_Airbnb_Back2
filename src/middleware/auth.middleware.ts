866915297069100a7aeb3dc71cf7ca44fd66d075
Anderson Moura <86577024+AndersonMoura333@users.noreply.github.com>
2021-11-28 12:27:25 -0300

    Update auth.middleware.ts

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -4,7 +4,7 @@ import { Response, Request, NextFunction } from 'express';
 export const authorizationMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const authorizationHeader = req.headers.authorization;
     if (authorizationHeader) {
-        verify(authorizationHeader, '9B20AC9338D7F324E6743676B6331FE135B96CB0C676D0D6720460B984B486FA', (err, decoded) => {
+        verify(authorizationHeader, process.env.HASH_KEY!, (err, decoded) => {
             if (err) {
                 res.status(401).json({
                     message: 'token invalido'
